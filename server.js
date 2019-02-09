acccbec3fc2bfad4467791706bbd3cc6cc7a07c4
michabeeri <michaelb@wix.com>
2019-02-09 20:00:21 +0200

    updating gitignore

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static(__dirname))
 app.all('*', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
 app.listen(8080)
 
-console.log("Running at Port 8080")
+console.log("Running on Port 8080")
